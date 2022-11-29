c998275b8e64aaddf90872a32a47a5637e44b044
samwhelp <samwhelp@users.noreply.github.com>
2022-11-29 09:33:14 +0800

    fix

---
+++ _demo/howto/config-wallpaper-by-command/config-wallpaper-by-plasma-apply-wallpaperimage/config-install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+
+kde_config_install_config_wallpaper () {
+
+
+	#local image_file_path='/usr/share/wallpapers/SafeLanding/'
+
+	local image_file_path='/usr/share/wallpapers/FallenLeaf/'
+
+	plasma-apply-wallpaperimage "${image_file_path}"
+
+
+}
+
+
+
+
+kde_config_install_config_wallpaper
+
